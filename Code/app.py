f10e920dfd8024fe2bbfc7d99e90e90a217e1b77
Lar <so_orr@mail.ru>
2020-08-02 01:45:17 +0300

    Add analysis download

--- Code/app.py
+++ Code/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, abort, make_response, Response
+from util import file_reader
 
 import config
 
@@ -14,17 +15,38 @@ def main_page():
 def process_data():
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
-    format = request.content_type
-
+    format = request.args.get('format')
+    # TODO choose file depending on Google/iOS
+    format_getter = None
     # start_date = request.form('start_date')
     # end_date = request.form('end-date')
-    if start_date is None or end_date is None:
-        return "Please choose both dates"
+    if start_date is None or start_date == '':
+        # Is smaller than any number char
+        start_date = '0'
+    if end_date is None or end_date == '':
+        # Is larger than any number char
+        end_date = 'INF'
+    # TODO check dateformat
     if start_date > end_date:
-        return "Start date should be less or equal end date"
-
-    return jsonify({"res": start_date + " " + end_date})
-
+        abort(400)
+        abort(Response('Start date should be less or equal end date'))
+    # TODO format in header
+    if format == 'CSV':
+        format_getter = file_reader.get_csv
+    elif format == 'JSON':
+        format_getter = file_reader.get_json
+    else:
+        abort(400, Response('No such format'))
+    res = format_getter(start_date, end_date)
+    output = make_response(res)
+    if format == 'CSV':
+        output.headers["Content-Disposition"] = "attachment; filename=reviews_" + \
+                                                start_date + "_to_" + end_date + ".csv"
+        output.headers["Content-type"] = "text/csv"
+    elif format == 'JSON':
+        # output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        output.headers["Content-type"] = "application/json"
+    return output
 
 if __name__ == '__main__':
     app.run(config.address, port=config.port, debug=config.debug)
---
+++ Code/util/file_reader.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# TODO move names to config?
+file_path = '../Resources/processed_reviews.tsv'
+date_name = 'Date'
+
+
+def get_reviews_df(start_date: str, end_date: str) -> pd.DataFrame:
+    # TODO cache reviews
+    reviews = pd.read_csv(file_path, sep='\t')
+    reviews = reviews[reviews[date_name].between(start_date, end_date)]
+    return reviews
+
+
+def get_json(start_date, end_date):
+    return get_reviews_df(start_date, end_date).to_json(force_ascii=False, orient='records')
+
+
+def get_csv(start_date, end_date):
+    # TODO fix possible memory error
+    return get_reviews_df(start_date, end_date).to_csv(sep='\t')
+
+
+def get_type_map(start_date, end_date):
+    return get_reviews_df(start_date, end_date).groupby('Intention').count()['Link'].to_dict()
