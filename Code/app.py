9cbf588df2d9ceb742cec7298850a7cd059bd5de
Lar <so_orr@mail.ru>
2020-08-01 17:55:51 +0300

    Add Bootstrap

--- Code/app.py
+++ Code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 import config
 
@@ -14,11 +14,16 @@ def main_page():
 def process_data():
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
+    format = request.content_type
+
     # start_date = request.form('start_date')
     # end_date = request.form('end-date')
     if start_date is None or end_date is None:
         return "Please choose both dates"
-    return start_date + " " + end_date
+    if start_date > end_date:
+        return "Start date should be less or equal end date"
+
+    return jsonify({"res": start_date + " " + end_date})
 
 
 if __name__ == '__main__':
